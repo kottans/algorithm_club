ab9493ea22ca04a8e05f65c64add959d1a993340
Quodnon <feanorfeanturi@gmail.com>
2018-02-24 15:11:05 +0200

    testing configs

--- tasks/quodnon/week2/quick_fun/Program.cs
+++ tasks/quodnon/week2/quick_fun/Program.cs
@@ -6,7 +6,7 @@ namespace quick_fun
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! with good git config ");
         }
     }
 }

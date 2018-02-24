5bc772c2b63716c61a7a87230c6ae4555ade9f6e
Quodnon <feanorfeanturi@gmail.com>
2018-02-24 15:15:24 +0200

    start quicksort task

--- tasks/quodnon/Readme.md
+++ tasks/quodnon/Readme.md
@@ -4,3 +4,5 @@
 - week1 ()
     - count inversions using C# and `refs`
     - count inversions using JS and `while loop with returning objects`
+- week 2 ()
+    - quicksort using pivots
--- tasks/quodnon/week2/quick_fun/Program.cs
+++ tasks/quodnon/week2/quick_fun/Program.cs
@@ -8,5 +8,15 @@ namespace quick_fun
         {
             Console.WriteLine("Hello World! with good git config ");
         }
+
+        static enum pivotPoint{
+            Last:0,
+            First:1,
+            Middle:2
+        }
+        static int[] QuickSort (int [] array, enum pivotPoint)
+        {
+            swi
+        }
     }
 }

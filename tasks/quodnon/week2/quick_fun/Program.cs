2336b14d4588cec0d3a28369b954e8e353cace2b
Quodnon <feanorfeanturi@gmail.com>
2018-02-24 15:09:13 +0200

    basic setup for week 2

---
+++ tasks/quodnon/week2/quick_fun/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace quick_fun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

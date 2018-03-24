2f551e7a8df3a55dcc5a2506bafff531cd75f619
Quodnon <feanorfeanturi@gmail.com>
2018-03-24 12:44:54 +0200

    second attemtp to solve quicksort issue

--- tasks/quodnon/week2/quick_fun/Program.cs
+++ tasks/quodnon/week2/quick_fun/Program.cs
@@ -12,19 +12,34 @@ namespace quick_fun
         {
             Console.WriteLine("Hello World! with good git config ");
             var unsortedArr = File.ReadAllText("QuickSort.txt").Split('\n').Select(x => Int32.Parse(x)).ToArray();
-            
+            /*
+             * get array
+             * put it in sorter
+             * put in counter in sorter
+             * inside sorter:
+             * - init 2 indexes
+             * - take first element
+             * - swap swap swap
+             * .
+             * get result from sorter
+             */
+            Console.WriteLine("Gello, worldd{0}", 1);
 
         }
 
-        enum pivotPoint{
-            Last=0,
-            First=1,
-            Middle=2
+        enum pivotPoint {
+            Last = 0,
+            First = 1,
+            Middle = 2
         }
-
-        static int[] QuickSort (int [] array, enum pivotPoin, out int counter)
+        int [] partition(int[] array,
+        int[] QuickSort(int[] array, pivotPoint point, out int counter)
         {
-           
+            var s = new int[] { 1, 2, 3, 4 };
+            counter = 10;
+
+
+            return s;
         }
     }
 }

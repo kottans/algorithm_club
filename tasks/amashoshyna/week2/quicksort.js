1b450ca21ee884f7c5c7d2059c641cd0bff48902
amashoshyna <a.mashoshyna@gmail.com>
2018-03-27 01:20:55 +0300

    quicksort refactor

--- tasks/amashoshyna/week2/quicksort.js
+++ tasks/amashoshyna/week2/quicksort.js
@@ -1,4 +1,4 @@
-
+let m = 0
 function choosePivot(arr, limits) {
 	if(limits) {
 		return Math.floor(
@@ -6,6 +6,20 @@ function choosePivot(arr, limits) {
 		) 
 	} 
 	return Math.floor(Math.random()*arr.length)
+}
+
+const pivotChoiceStrategies = {
+    random: (arr, limits) => {
+        if(limits) {
+            return Math.floor(
+                Math.random() * (limits[1] - limits[0])+ limits[0]
+            )
+        }
+        return Math.floor(Math.random()*arr.length)
+    },
+    alwaysFirst: (arr, limits) => {
+        return limits[0]
+    }
 }
 function swapByIndex(arr, a, b) {
 	let tempA = arr[a]
@@ -15,7 +29,7 @@ function swapByIndex(arr, a, b) {
 }
 
 function partition(arr, boundaries) {
-    let initialPivotIndex = choosePivot(arr, boundaries)
+    let initialPivotIndex = pivotChoiceStrategies.alwaysFirst(arr, boundaries)
     let [leftBoundary, rightBoundary] = boundaries
 	if(initialPivotIndex !== leftBoundary) {
 		swapByIndex(arr, leftBoundary, initialPivotIndex)
@@ -42,10 +56,23 @@ function quickSort(arr, boundariesGiven) {
 		let leftPartLimits = [boundaries[0], positionedElementIndex - 1]
 		let rightPartLimits = [positionedElementIndex + 1, boundaries[1]]
 		if (positionedElementIndex - 1 >= 0
-            && (leftPartLimits[1] - leftPartLimits[0]) > 0) quickSort(arr, leftPartLimits)
+            && (leftPartLimits[1] - leftPartLimits[0]) > 0) {
+                quickSort(arr, leftPartLimits)
+                m = m + (leftPartLimits[1] - leftPartLimits[0] - 1)
+            }
         if (positionedElementIndex + 1 <= boundaries[1]
-            && (rightPartLimits[1] - rightPartLimits[0]) > 0) quickSort(arr, rightPartLimits)
-	} 
+            && (rightPartLimits[1] - rightPartLimits[0]) > 0) {
+                quickSort(arr, rightPartLimits)
+                m = m + (rightPartLimits[1] - rightPartLimits[0]-1)
+            }
+    }
+    console.log(m)
 }
 
-module.exports = quickSort
+function quickSortByStrategy (strategy) {
+    // let strategy = strategy
+    let choosePivot = pivotChoiceStrategies[strategy]
+    return quickSort.bind(this)
+}
+
+module.exports = quickSortByStrategy

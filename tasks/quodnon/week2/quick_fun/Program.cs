be8cb9643fd92ab1ea32199f96f12d7d7caa05bb
Quodnon <feanorfeanturi@gmail.com>
2018-04-03 02:29:12 +0300

    finally i did it week 2

--- tasks/quodnon/week2/quick_fun/Program.cs
+++ tasks/quodnon/week2/quick_fun/Program.cs
@@ -8,22 +8,19 @@ namespace quick_fun {
         static void Main (string[] args) {
             Console.WriteLine ("Hello World! with good git config ");
             var unsortedArr = File.ReadAllText ("QuickSort.txt").Split ('\n').Select (x => Int32.Parse (x)).ToArray ();
-            /*
-             * get array
-             * put it in sorter
-             * put in counter in sorter
-             * inside sorter:
-             * - init 2 indexes
-             * - take first element
-             * - swap swap swap
-             * .
-             * get result from sorter
-             */
+            
             var s = new Quicksort (new int[] { 7, 6, 3, 4, 5, 2, 1 });
+            var r = new Quicksort(unsortedArr);
             Console.WriteLine ("Gello, worldd{0}", 1);
 
         }
 
+    }
+    enum  SMode
+    {
+        first=1,
+        last = 2,
+        median =3
     }
     class Quicksort {
         private int[] arr { get; set; }
@@ -35,22 +32,40 @@ namespace quick_fun {
         }
         public int counter = 0;
 
-        public Quicksort (int[] arrinput) {
+        public Quicksort (int[] arrinput, SMode mode = SMode.first ) {
             this.arr = arrinput;
             this.counter = 0;
-            qs(arr,0,arr.Length-1);
-            Console.WriteLine(string.Join(',',arr));
+            qs(arr,0,arr.Length);
+            Console.WriteLine(string.Join(',',this.arr));
+            Console.WriteLine(this.counter);
         }
         private void qs (int[] arr, int left, int right) {
-            Console.WriteLine("{0}/{1}",left,right);
-            Console.WriteLine(string.Join(',',arr));
+           // Console.WriteLine("{0}/{1}",left,right);
+            //Console.WriteLine(string.Join(',',arr));
             
             int swapBoundary = left;
             int partitionMarker = left;
+#region  2 task last index
+          //  swap(right-1,left);
+#endregion
+
+#region 3 task median index
+        var middleIndex = (left-1+right)/2;
+        var a1 = arr[left];
+        var a2 = arr[right-1];
+        var a3 = arr[middleIndex];
+    if((a1<a2 && a1>a3)||(a1>a2 && a1<a3)){        
+        //nothing
+    } else    if ((a2<a1 && a2>a3)||(a2>a1 && a2<a3)){
+        swap(left,right-1);        
+    } else {
+        swap(left,middleIndex);        
+    }
+#endregion
             int zeroEl = arr[left]; // replace with function
             this.counter += (right - left - 1);
 
-            while (partitionMarker <= right) {
+            while (partitionMarker < right) {
                 if (arr[partitionMarker] <= zeroEl) {
                     swap (partitionMarker, swapBoundary);
                     swapBoundary++;
@@ -58,16 +73,16 @@ namespace quick_fun {
                 partitionMarker++;
             }
             //final SWAP
-            swap (left, swapBoundary);
+            swap (left, swapBoundary-1);
 
             //2 rec calls
             if (swapBoundary - 1-left > 1) {
-                Console.WriteLine("left {0}-{1}",left,swapBoundary-1);
+               // Console.WriteLine("left {0}-{1}",left,swapBoundary-1);
                 qs (this.arr, left, swapBoundary - 1);
             }
             if (right - swapBoundary > 1) {
-                Console.WriteLine("right {0}-{1}",swapBoundary+1,right);
-                qs (this.arr, swapBoundary + 1, right);
+                //Console.WriteLine("right {0}-{1}",swapBoundary+1,right);
+                qs (this.arr, swapBoundary , right);
             }
         }
 
